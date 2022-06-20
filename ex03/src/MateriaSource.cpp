4abe121714643de19efe957258b01fbd00991205
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-06-20 15:39:03 +0900

    fix: copy constuctor

--- ex03/src/MateriaSource.cpp
+++ ex03/src/MateriaSource.cpp
@@ -2,15 +2,21 @@
 
 #include <string>
 
-MateriaSource::MateriaSource(void) {
+void MateriaSource::initMaterias() {
     for (int i = 0; i < kMaxSize; ++i) {
         materias_[i] = NULL;
     }
 }
 
-// MateriaSource::MateriaSource(const MateriaSource& other) {
-//     *this = other;
-// }
+
+MateriaSource::MateriaSource(void) {
+    initMaterias();
+}
+
+MateriaSource::MateriaSource(const MateriaSource& other) {
+    initMaterias();
+    *this = other;
+}
 
 MateriaSource::~MateriaSource(void) {
     for (int i = 0; i < kMaxSize; ++i) {
@@ -18,9 +24,17 @@ MateriaSource::~MateriaSource(void) {
     }
 }
 
-// MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
-//     return *this;
-// }
+MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
+    for (int i = 0; i < kMaxSize; ++i) {
+        delete this->materias_[i];
+        if (other.materias_[i] == NULL) {
+            this->materias_[i] = NULL;
+            continue;
+        }
+        this->materias_[i] = other.materias_[i]->clone();
+    }
+    return *this;
+}
 
 void MateriaSource::learnMateria(AMateria* m) {
     if (m == NULL) return;
@@ -39,7 +53,6 @@ AMateria* MateriaSource::createMateria(std::string const & type) {
             continue;
         }
         if (materias_[i]->getType() == type) {
-            // return new AMateria(type);
             return materias_[i]->clone();
         }
     }
--- ex03/src/MateriaSource.hpp
+++ ex03/src/MateriaSource.hpp
@@ -4,17 +4,19 @@
 #include "IMateriaSource.hpp"
 #include "AMateria.hpp"
 
+// TODO Characterみたいにdeep copyを実装すべきか
 class MateriaSource : public IMateriaSource {
  private:
     static const int kMaxSize = 4;
     AMateria *materias_[kMaxSize];
 
+    void initMaterias();
+
  public:
     MateriaSource();
     ~MateriaSource();
-    // MateriaSource(const MateriaSource& other);
-    // ~MateriaSource(void);
-    // MateriaSource &operator=(const MateriaSource& other);
+    MateriaSource(const MateriaSource& other);
+    MateriaSource &operator=(const MateriaSource& other);
 
     void learnMateria(AMateria*);
     AMateria* createMateria(std::string const & type);
